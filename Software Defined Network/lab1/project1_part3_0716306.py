2a22e5c675bfdbc1eb6c0e5297808f2792cb9d87
Ed-Ko <65457924+Ed-Ko@users.noreply.github.com>
2022-02-18 10:49:08 +0800

    upload lab1 code

---
+++ Software Defined Network/lab1/project1_part3_0716306.py
@@ -0,0 +1,28 @@
+from mininet.topo import Topo
+
+class Project1_Topo_0716306(Topo):
+  def __init__(self):
+    Topo.__init__(self)
+
+    h1 = self.addHost('h1',ip='192.168.0.1/27')
+    h2 = self.addHost('h2',ip='192.168.0.2/27')
+    h3 = self.addHost('h3',ip='192.168.0.3/27')
+
+    s1 = self.addSwitch('s1')
+    s2 = self.addSwitch('s2')
+    s3 = self.addSwitch('s3')
+    s4 = self.addSwitch('s4')
+
+    self.addLink(h1,s1)
+    self.addLink(h2,s2)
+    self.addLink(h3,s3)
+    self.addLink(s1,s4)
+    self.addLink(s2,s4)
+    self.addLink(s3,s4)
+
+topos = {'topo_part3_0716306':Project1_Topo_0716306}
+
+
+
+
+
